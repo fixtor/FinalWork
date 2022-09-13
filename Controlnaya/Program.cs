8feee3c268c98c3989269665e242d870cf68dc72
Igor_U <fixtor75@gmail.com>
2022-09-13 19:10:42 +0300

    Создали метод для определения слов из двух символов

--- Controlnaya/Program.cs
+++ Controlnaya/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void GetCharCount(string array)
+{
+
+    string stringArray = array;
+    char[] temp = new char[stringArray.Length];
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        temp[i] = stringArray[i];
+        Console.WriteLine($"{stringArray[i]} = {temp[i]}");
+    }
+
+}
+//System.Console.WriteLine("Введите слова:");
+
+
+string GetWordsStringArray(string[] array)
+{
+    string words = "\0";
+    string wordsTwoSymbols = words;
+    for (int i = 0; i < array.Length; i++)
+    {
+        words = array[i];
+        if (words.Length < 3)
+        {
+            wordsTwoSymbols += words + " ";
+        }
+    }
+    return wordsTwoSymbols;
+}
+
+string[] strArray = { "Привет", "он", "ко", "жесть" };
+string ab = GetWordsStringArray(strArray);
+System.Console.WriteLine($"{ab}");
+//GetCharCount(ab);
