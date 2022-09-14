e83bf0d4b50e5ffa11e0d4a1244bff1ee8b32490
Igor_U <fixtor75@gmail.com>
2022-09-14 14:29:45 +0300

    Итоговое задание готово

--- Controlnaya/Program.cs
+++ Controlnaya/Program.cs
@@ -1,17 +1,38 @@
-﻿string GetWordsStringArray(string[] array)
+﻿string[] strArray = { "Пт", "-2", ":)", "1112", "11", "Dj,", "Programming" }; // Введем случайный массив строк
+
+// string[] strArray = { "hello", "2", "world", ":-)" };
+//string[] strArray = { "1234", "1567", "-2", "computer science" };
+//string[] strArray = { "Russia", "Denmark", "Kazan" };
+
+string[] ab = GetWordsStringArray(strArray); // создадим массив, который формируется из метода выбора коротких значений по условию
+System.Console.Write("[");
+for (int i = 0; i < ab.Length; i++)
+    System.Console.Write($" {ab[i]} ");
+System.Console.WriteLine("]");
+
+string[] GetWordsStringArray(string[] array)
 {
-    string words = "\0";
-    string wordsTwoSymbols = words;
+    string word = "\0";
+    int count = 0; // Переменная для создания нового массива строк
     for (int i = 0; i < array.Length; i++)
     {
-        words = array[i];
-        if (words.Length < 3)
-            wordsTwoSymbols += words + " ";
+        word = array[i];
+        if (word.Length <= 3)
+        {
+            count++;
+        }
     }
-    return wordsTwoSymbols;
-}
 
-string[] strArray = { "Привет", "он", "ко", "жесть" };
-string ab = GetWordsStringArray(strArray);
-System.Console.WriteLine($"{ab}");
-//GetCharCount(ab);
+    string[] finalArray = new string[count];
+    count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        word = array[i];
+        if (word.Length <= 3)
+        {
+            finalArray[count] = array[i];
+            count++;
+        }
+    }
+    return finalArray;
+}
